51ec103c7e6ef72f90f0877bf2f636b7f7c028d6
jianhui1012 <golikeholdon@gmail.com>
2018-01-17 00:07:21 +0800

    init project

---
+++ ViewPagerPage.js
@@ -0,0 +1,85 @@
+/**
+ * Created by golike on 2018/1/16.
+ */
+import {StyleSheet, View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {PagerTabIndicator, IndicatorViewPager, PagerTitleIndicator, PagerDotIndicator} from 'rn-viewpager';
+
+export default class ViewPagerPage extends Component {
+    render() {
+        return (
+            <View style={{flex:1}}>
+                <IndicatorViewPager
+                    style={{height:200}}
+                    indicator={this._renderDotIndicator()}
+                >
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                </IndicatorViewPager>
+
+                <IndicatorViewPager
+                    style={{flex:1, paddingTop:20, backgroundColor:'white'}}
+                    indicator={this._renderTitleIndicator()}
+                >
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                </IndicatorViewPager>
+
+                <IndicatorViewPager
+                    style={{flex:1, paddingTop:20, backgroundColor:'white'}}
+                    indicator={this._renderTabIndicator()}
+                >
+                    <View style={{backgroundColor:'cadetblue'}}>
+                        <Text>page one</Text>
+                    </View>
+                    <View style={{backgroundColor:'cornflowerblue'}}>
+                        <Text>page two</Text>
+                    </View>
+                    <View style={{backgroundColor:'#1AA094'}}>
+                        <Text>page three</Text>
+                    </View>
+                </IndicatorViewPager>
+            </View>
+        );
+    }
+
+    _renderTitleIndicator() {
+        return <PagerTitleIndicator titles={['one', 'two', 'three']} />;
+    }
+
+    _renderDotIndicator() {
+        return <PagerDotIndicator pageCount={3} />;
+    }
+
+    _renderTabIndicator() {
+        let tabs = [{
+            text: 'Home',
+            iconSource: require('../imgs/ic_tab_home_normal.png'),
+            selectedIconSource: require('../imgs/ic_tab_home_click.png')
+        },{
+            text: 'Message',
+            iconSource: require('../imgs/ic_tab_task_normal.png'),
+            selectedIconSource: require('../imgs/ic_tab_task_click.png')
+        },{
+            text: 'Profile',
+            iconSource: require('../imgs/ic_tab_my_normal.png'),
+            selectedIconSource: require('../imgs/ic_tab_my_click.png')
+        }];
+        return <PagerTabIndicator tabs={tabs} />;
+    }
+
+}
